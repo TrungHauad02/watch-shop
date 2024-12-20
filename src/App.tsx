e62730f221534ae2837839ce277e8784aa07da22
Nguyen Trung Hau <trunghauad02@gmail.com>
2024-12-20 19:17:06 +0700

    C(globel.d.ts)

--- src/App.tsx
+++ src/App.tsx
@@ -6,25 +6,29 @@ import { ThemeProvider } from "@mui/material";
 import theme from "./theme";
 import "react-toastify/dist/ReactToastify.css";
 import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   const isDarkMode = useSelector((state: RootState) => state.theme.isDarkMode);
 
   return (
-    <BrowserRouter>
-      <ThemeProvider theme={theme(isDarkMode)}>
-        <AppRoutes />
-        <ToastContainer
-          position="top-right"
-          autoClose={3000}
-          hideProgressBar={false}
-          closeOnClick
-          pauseOnHover
-          aria-label="Toast container"
-          theme={isDarkMode ? "dark" : "light"}
-        />
-      </ThemeProvider>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <ThemeProvider theme={theme(isDarkMode)}>
+          <AppRoutes />
+          <ToastContainer
+            position="top-right"
+            autoClose={3000}
+            hideProgressBar={false}
+            closeOnClick
+            pauseOnHover
+            aria-label="Toast container"
+            theme={isDarkMode ? "dark" : "light"}
+          />
+        </ThemeProvider>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
-import store from "./redux/store";
 import "./index.css";
 import App from "App";
 
@@ -9,9 +7,7 @@ const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <div>
+    <App />
+  </div>
 );
